6564fbb010b2c03ad67aac5333919e8df0e1b413
Sumit21067 <150153790+Sumit21067@users.noreply.github.com>
2023-12-28 19:41:08 +0530

    Add files via upload

---
+++ Break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0;i<20;i++)
+    {
+        
+        if(i==15)
+        {
+            break;
+        }
+        printf("%d",i);
+        
+    }
+}
